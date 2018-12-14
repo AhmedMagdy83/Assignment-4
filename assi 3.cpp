80e2f9fcde5567ffd2002e9a30d98af4092fede8
islam saleh <44006288+islam223@users.noreply.github.com>
2018-12-14 20:11:09 +0200

    Add files via upload

---
+++ assi 3.cpp
@@ -0,0 +1,26 @@
+// assi 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include"Sequence.h"
+#include"DNA.h"
+using namespace std;
+int main()
+{
+	Sequence s;
+	cin >> s;
+	Sequence s1;
+	s1 = s;
+	s1.Print();
+	/*
+	DNA d;
+	cin>> d;
+	//d.ConvertToRNA();;
+	DNA D2;
+	D2 = d;
+    D2.Print();
+	*/
+	 
+	}
